f390ded981779fffbf07602d262c1d798022ee44
seb-kvist <sebastian_kvist@hotmail.com>
2025-01-06 13:32:20 +0100

    Fixat kommentarer på Auctionitem

--- Models/AuctionItem.cs
+++ Models/AuctionItem.cs
@@ -5,16 +5,15 @@ namespace AuktionApp.Models;
 public class AuctionItem
 {
     public string Id { get; set; } // Format: Tre bokstäver och sex siffror
-    public string Name { get; set; }
-    public string Decade { get; set; }
-    public string Description { get; set; }
-    public string Category { get; set; }
+    public string Name { get; set; } //Namn på item
+    public string Decade { get; set; } //Årtionde för intern revision
+    public string Description { get; set; } //Produktbeskr
+    public string Category { get; set; } //Kategori
 
-    public decimal CostPrice { get; set; }
-    public decimal StartingPrice { get; set; }
-    public decimal? FinalPrice { get; set; }
+    public decimal StartingPrice { get; set; } //Startpris
+    public decimal? FinalPrice { get; set; } //Slutpris
     
     public string Status { get; set; } // "Tillgänglig" eller "Såld"
-    public string CreatedById { get; set; }
-    public IdentityUser CreatedBy { get; set; }
+    public string CreatedById { get; set; } //Id på användare som skapat auktionen
+    public IdentityUser CreatedBy { get; set; } //Användare som skapat auktionen
 }
